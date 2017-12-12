28677b8375eba6e4c4c89ad9044043c0c6648042
Tchaka Spierings <tchakas@bottomline-group.com>
2017-12-12 17:57:45 +0100

    Implement calendar days

--- src/app/modules/timesheet/timesheet.module.ts
+++ src/app/modules/timesheet/timesheet.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { TimesheetComponent } from './timesheet/timesheet.component';
 
 @NgModule({
   imports: [
     CommonModule
+  ],
+  providers: [
+    DatePipe
   ],
   declarations: [TimesheetComponent]
 })
--- src/app/modules/timesheet/timesheet/timesheet.component.ts
+++ src/app/modules/timesheet/timesheet/timesheet.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+export interface Day {
+  day: Date;
+}
 
 @Component({
   selector: 'app-timesheet',
@@ -7,11 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimesheetComponent implements OnInit {
 
-  days = new Array(31);
+  days = new Array(42);
 
-  constructor() { }
+  constructor(private datePipe: DatePipe) { }
 
   ngOnInit() {
+    this.fillCalendar();
+  }
+
+  private fillCalendar() {
+    const thisYear = new Date().getFullYear();
+    const thisMonth = new Date().getMonth();
+    const firstDay = new Date(thisYear, thisMonth);
+
+    console.log(firstDay.getDay());
+
+    // Edge case: show week before if month starts on monday
+    let startDayOfWeek = firstDay.getDay();
+    startDayOfWeek = startDayOfWeek <= 1 ? startDayOfWeek + 7 : startDayOfWeek;
+
+    let dayCursor = new Date(thisYear, thisMonth, 1);
+    for (let i = 0; dayCursor.getMonth() === thisMonth; i++) {
+      this.days[startDayOfWeek + i - 1] = {
+        day: dayCursor
+       };
+
+      dayCursor = new Date(thisYear, thisMonth, i + 2);
+    }
   }
 
 }
